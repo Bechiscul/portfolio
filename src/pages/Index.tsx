0d099a1f02bd4dfb14caea904c860c1337908046
sebastian <sebberas2@gmail.com>
2021-06-07 23:45:51 +0200

    subtitle

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,6 +13,7 @@ const Index = () => {
                     <div>
                         <p className="font-roboto text-gray-500 uppercase tracking-wide">Developer & Student</p>
                         <h1 className="text-5xl font-roboto">Hello! I'm Sebastian.</h1>
+                        <p className="mt-4 font-roboto">I enjoy spending my time writing game engines.</p>
                     </div>
                 </div>
             </div>
